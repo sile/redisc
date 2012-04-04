538ff837f2f773b6638b2d5a50dd8deb3b481292
Takeru Ohta <phjgt308@gmail.com>
2012-04-05 02:08:49 +0900

    defcmd2 => defcmd

--- command.lisp
+++ command.lisp
@@ -9,6 +9,7 @@
       (list (subseq name 0 p) (subseq name (1+ p))))))
 
 ;; TODO: redisのドキュメントを付与する
+#+C
 (defmacro defcmd (name arity result-type &key vary)
   (let ((args (loop REPEAT arity COLLECT (gensym))))
     ;; TODO: いろいろ整理
@@ -61,7 +62,7 @@
     (cons (car x) (ensure-list (cdr x)))))
 )
 
-(defmacro defcmd2 (category version name args result-type description)
+(defmacro defcmd (category version name args result-type description)
   (let ((connection (gensym))
         (vars (gensym))) ; XXX:
     `(progn 
@@ -112,30 +113,30 @@
     `(multi ,commands :connection ,connection)))
 
 ;; strings
-(defcmd2 :strings 2.0.0 :append (key value) :integer "Append a value to a key")
-(defcmd2 :strings 1.0.0 :decr (key) :integer "Decrement the integer value of a key by one")
-(defcmd2 :strings 1.0.0 :decrby (key decrement) :integer "Decrement the integer value of a key by the given number")
-(defcmd2 :strings 1.0.0 :get (key) :string "Get the value of a key")
-(defcmd2 :strings 2.2.0 :getbit (key offset) :integer "Returns the bit value at offset in the string value stored at key")
-(defcmd2 :strings 2.4.0 :getrange (key start end) :string "Get a substring of the string stored at a key")
-(defcmd2 :strings 1.0.0 :getset (key value) :string "Set the string value of a key and return its old value")
-(defcmd2 :strings 1.0.0 :incr (key) :integer "Increment the integer value of a key by one")
-(defcmd2 :strings 1.0.0 :incrby (key increment) :integer "Increment the integer value of a key by the given amount")
-(defcmd2 :strings 2.6.0 :incrbyfloat (key increment) :number "Increment the float value of a key by the given amount")
-(defcmd2 :strings 1.0.0 :mget (key . _) :list "Get the values of all the given keys")
-(defcmd2 :strings 1.0.1 :mset (key value . _) :true "Set multiple keys to multiple values")
-(defcmd2 :strings 1.0.1 :msetnx (key value . _) :boolean "Set multiple keys to multiple values, only if none of the keys exist")
-(defcmd2 :strings 2.6.0 :psetex (key milliseconds value) :true "Set the value and expiration in milliseconds of a key")
-(defcmd2 :strings 1.0.0 :set (key value) :true "Set the string value of a key")
-(defcmd2 :strings 2.2.0 :setbit (key offset value) :integer "Sets or clears the bit at offset in the string value stored at key")
-(defcmd2 :strings 2.0.0 :setex (key seconds value) :true "Set the value and expiration of a key")
-(defcmd2 :strings 2.2.0 :setrange (key offset value) :integer "Overwrite part of a string at key starting at the specified offset")
-(defcmd2 :strings 2.2.0 :strlen (key) :integer "Get the length of the value stored in a key")
+(defcmd :strings 2.0.0 :append (key value) :integer "Append a value to a key")
+(defcmd :strings 1.0.0 :decr (key) :integer "Decrement the integer value of a key by one")
+(defcmd :strings 1.0.0 :decrby (key decrement) :integer "Decrement the integer value of a key by the given number")
+(defcmd :strings 1.0.0 :get (key) :string "Get the value of a key")
+(defcmd :strings 2.2.0 :getbit (key offset) :integer "Returns the bit value at offset in the string value stored at key")
+(defcmd :strings 2.4.0 :getrange (key start end) :string "Get a substring of the string stored at a key")
+(defcmd :strings 1.0.0 :getset (key value) :string "Set the string value of a key and return its old value")
+(defcmd :strings 1.0.0 :incr (key) :integer "Increment the integer value of a key by one")
+(defcmd :strings 1.0.0 :incrby (key increment) :integer "Increment the integer value of a key by the given amount")
+(defcmd :strings 2.6.0 :incrbyfloat (key increment) :number "Increment the float value of a key by the given amount")
+(defcmd :strings 1.0.0 :mget (key . _) :list "Get the values of all the given keys")
+(defcmd :strings 1.0.1 :mset (key value . _) :true "Set multiple keys to multiple values")
+(defcmd :strings 1.0.1 :msetnx (key value . _) :boolean "Set multiple keys to multiple values, only if none of the keys exist")
+(defcmd :strings 2.6.0 :psetex (key milliseconds value) :true "Set the value and expiration in milliseconds of a key")
+(defcmd :strings 1.0.0 :set (key value) :true "Set the string value of a key")
+(defcmd :strings 2.2.0 :setbit (key offset value) :integer "Sets or clears the bit at offset in the string value stored at key")
+(defcmd :strings 2.0.0 :setex (key seconds value) :true "Set the value and expiration of a key")
+(defcmd :strings 2.2.0 :setrange (key offset value) :integer "Overwrite part of a string at key starting at the specified offset")
+(defcmd :strings 2.2.0 :strlen (key) :integer "Get the length of the value stored in a key")
 
 ;; transaction
-(defcmd2 :transaction 1.2.0 :exec () :list "Execute all commands issued after multi")
-(defcmd2 :transaction 1.2.0 :multi () :true "Mark the start of a transaction block")
-(defcmd2 :transaction 2.2.0 :watch (key . _) :true "Watch the given keys to determine execution of the MULTI/EXEC block")
+(defcmd :transaction 1.2.0 :exec () :list "Execute all commands issued after multi")
+(defcmd :transaction 1.2.0 :multi () :true "Mark the start of a transaction block")
+(defcmd :transaction 2.2.0 :watch (key . _) :true "Watch the given keys to determine execution of the MULTI/EXEC block")
 
 
 
